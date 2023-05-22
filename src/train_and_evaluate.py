2044e348e4bd5ad60b503f7042c8028facbed808
Prajwal <prajwalgbdr03@gmail.com>
2023-05-22 21:36:32 +0530

    train and evaluate

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -5,4 +5,30 @@
 
 import os
 import pandas as pd
+import sys
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+from get_data import read_params
+import argparse
+import joblib
+import json
 
+
+def train_and_evaluate(config_path):
+    pass
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    split_and_saved_data(config_path=parsed_args.config)
