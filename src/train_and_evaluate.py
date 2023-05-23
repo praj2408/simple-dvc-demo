65f2db94a49d1f43da9d02741e5487e940085aa0
Prajwal <prajwalgbdr03@gmail.com>
2023-05-23 10:44:11 +0530

    train and evaluate

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -16,14 +16,38 @@ import argparse
 import joblib
 import json
 
+def eval_metrics(actual, pred):
+    rmse = 
+    mae = 
+    r2 = 
+    return rmse, mae, r2
 
 def train_and_evaluate(config_path):
-    pass
+    config = read_params(config_path)
+    test_data_path = config["split_data"]["test_path"]
+    train_data_path = config["split_data"]["train_path"]
+    random_state = config["base"]["random_state"]
+    model_dir = config["model_dir"]
 
+    alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
+    l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
 
+    target = [config["base"]["target_col"]]
 
+    train = pd.read_csv(train_data_path, sep=',')
+    test = pd.read_csv(test_data_path, sep=',')
 
+    train_y = train[target]
+    test_y = test[target]
+    
+    train_X = train.drop(target, axis=1)
+    test_X = test.drop(target, axis=1)
 
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
+    lr.fit(train_X, train_y)
+
+    predicted_qualiies = lr.predict(test_X)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualiies)
 
 
 
