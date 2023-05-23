f446f11cf99d3133c3341abc913539f2be2f6c8e
Prajwal <prajwalgbdr03@gmail.com>
2023-05-23 13:27:08 +0530

    tracker added

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -17,9 +17,9 @@ import joblib
 import json
 
 def eval_metrics(actual, pred):
-    rmse = 
-    mae = 
-    r2 = 
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
     return rmse, mae, r2
 
 def train_and_evaluate(config_path):
@@ -49,10 +49,36 @@ def train_and_evaluate(config_path):
     predicted_qualiies = lr.predict(test_X)
     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualiies)
 
+    print("Elastic model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print(" RMSE: %s" %rmse)
+    print(" MAE: %s" % mae)
+    print(" R2: %s" % r2)
 
+    scores_file = config["reports"]["scores"]
+    params_file = config["reports"]["params"]
+
+    with open(scores_file, "w") as f:
+        scores = {
+            "rmse": rmse,
+            "mae": mae,
+            "r2": r2
+        }
+        json.dump(scores, f, indent=4)
+
+    with open(params_file, "w") as f:
+        params = {
+            "alpha": alpha,
+            "l1_ratio": l1_ratio
+        }
+        json.dump(params, f, indent=4)
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
+
+    joblib.dump(lr, model_path)
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    split_and_saved_data(config_path=parsed_args.config)
+    train_and_evaluate(config_path=parsed_args.config)
